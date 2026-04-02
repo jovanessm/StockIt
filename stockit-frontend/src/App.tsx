e7d53a389f8d3eafa525aa78162926abd51f4561
jovanessm <mjovaness@gmail.com>
2026-04-02 20:27:09 +0200

    feat(Dependencies): add React Router

--- stockit-frontend/src/App.tsx
+++ stockit-frontend/src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="justify-center align-center text-5xl">
-        Hello World!
-    </div>
-  )
-}
+import {Routes, Route} from "react-router-dom";
+import {LoginPage} from "@/pages/LoginPage.tsx";
 
-export default App
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<div>Home</div>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+        </Routes>
+    )
+}

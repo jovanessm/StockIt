f9f64727c3fdcd82bf16b39368ce440d28916bb0
jovanessm <mjovaness@gmail.com>
2026-04-03 15:34:09 +0200

    feat: enhance DashboardPage with chart components and update layout; add recharts dependency

--- stockit-frontend/src/app/DashboardPage.tsx
+++ stockit-frontend/src/app/DashboardPage.tsx
@@ -1,12 +1,25 @@
-import { SectionCards } from "@/components/dashboard/section-cards.tsx"
+import {SectionCards} from "@/components/dashboard/section-cards.tsx"
+import {ChartAreaLegend} from "@/components/dashboard/chart-area.tsx";
+import {ChartPieLabelCustom} from "@/components/dashboard/chart-pie.tsx";
 
 export default function DashboardPage() {
     return (
         <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2">
-                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                    <SectionCards />
+            <div className="@container/main flex flex-1 flex-col gap-4 py-6">
+                <div className="flex flex-col gap-6 md:gap-4">
+                    <SectionCards/>
                 </div>
+                <div className="flex flex-1 flex-row gap-4 px-4 md:gap-6 lg:px-6">
+                    <div className="w-2/9">
+                        <ChartAreaLegend/>
+                    </div>
+                </div>
+                <div className="flex flex-1 flex-row gap-4 px-4 md:gap-6 lg:px-6">
+                    <div className="w-1/5">
+                        <ChartPieLabelCustom/>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
--- stockit-frontend/src/components/dashboard/section-cards.tsx
+++ stockit-frontend/src/components/dashboard/section-cards.tsx
@@ -1,39 +1,32 @@
-import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
+import {IconTrendingDown, IconTrendingUp} from "@tabler/icons-react"
 
-import { Badge } from "@/components/ui/badge.tsx"
+import {Badge} from "@/components/ui/badge.tsx"
 import {
     Card,
     CardAction,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card.tsx"
 
 export function SectionCards() {
     return (
-        <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
+        // TODO(Jovan, 03.04.2026): Refactor to map with data const instead of hardcoded value
+        <div
+            className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-5 dark:*:data-[slot=card]:bg-card">
             <Card className="@container/card">
                 <CardHeader>
                     <CardDescription>Total Revenue</CardDescription>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[200px]/card:text-2xl">
                         $1,250.00
                     </CardTitle>
                     <CardAction>
                         <Badge variant="outline">
-                            <IconTrendingUp />
+                            <IconTrendingUp/>
                             +12.5%
                         </Badge>
                     </CardAction>
                 </CardHeader>
-                <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                    <div className="line-clamp-1 flex gap-2 font-medium">
-                        Trending up this month <IconTrendingUp className="size-4" />
-                    </div>
-                    <div className="text-muted-foreground">
-                        Visitors for the last 6 months
-                    </div>
-                </CardFooter>
             </Card>
             <Card className="@container/card">
                 <CardHeader>
@@ -43,19 +36,11 @@ export function SectionCards() {
                     </CardTitle>
                     <CardAction>
                         <Badge variant="outline">
-                            <IconTrendingDown />
+                            <IconTrendingDown/>
                             -20%
                         </Badge>
                     </CardAction>
                 </CardHeader>
-                <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                    <div className="line-clamp-1 flex gap-2 font-medium">
-                        Down 20% this period <IconTrendingDown className="size-4" />
-                    </div>
-                    <div className="text-muted-foreground">
-                        Acquisition needs attention
-                    </div>
-                </CardFooter>
             </Card>
             <Card className="@container/card">
                 <CardHeader>
@@ -65,17 +50,11 @@ export function SectionCards() {
                     </CardTitle>
                     <CardAction>
                         <Badge variant="outline">
-                            <IconTrendingUp />
+                            <IconTrendingUp/>
                             +12.5%
                         </Badge>
                     </CardAction>
                 </CardHeader>
-                <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                    <div className="line-clamp-1 flex gap-2 font-medium">
-                        Strong user retention <IconTrendingUp className="size-4" />
-                    </div>
-                    <div className="text-muted-foreground">Engagement exceed targets</div>
-                </CardFooter>
             </Card>
             <Card className="@container/card">
                 <CardHeader>
@@ -85,17 +64,25 @@ export function SectionCards() {
                     </CardTitle>
                     <CardAction>
                         <Badge variant="outline">
-                            <IconTrendingUp />
+                            <IconTrendingUp/>
                             +4.5%
                         </Badge>
                     </CardAction>
                 </CardHeader>
-                <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                    <div className="line-clamp-1 flex gap-2 font-medium">
-                        Steady performance increase <IconTrendingUp className="size-4" />
-                    </div>
-                    <div className="text-muted-foreground">Meets growth projections</div>
-                </CardFooter>
+            </Card>
+            <Card className="@container/card">
+                <CardHeader>
+                    <CardDescription>Growth Rate</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                        4.5%
+                    </CardTitle>
+                    <CardAction>
+                        <Badge variant="outline">
+                            <IconTrendingUp/>
+                            +4.5%
+                        </Badge>
+                    </CardAction>
+                </CardHeader>
             </Card>
         </div>
     )

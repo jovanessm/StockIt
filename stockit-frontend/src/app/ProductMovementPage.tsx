e59412635889cb5e87540d2d495e8005273bca76
jovanessm <mjovaness@gmail.com>
2026-04-17 09:22:24 +0200

    feat: add search input to ProductMovementPage for product filtering

--- stockit-frontend/src/app/ProductMovementPage.tsx
+++ stockit-frontend/src/app/ProductMovementPage.tsx
@@ -1,7 +1,22 @@
-export default function ProductMovementPage() {
-    return(
-        <div>
+import {Field} from "@/components/ui/field.tsx";
+import {InputGroup, InputGroupAddon, InputGroupInput} from "@/components/ui/input-group.tsx";
+import {SearchIcon} from "lucide-react";
 
+export default function ProductMovementPage() {
+    return (
+        <div className="flex flex-row">
+            <div className="lg:w-[70%]">
+                <Field>
+                    <InputGroup className="w-full">
+                        <InputGroupInput
+                            placeholder="Filter products..."
+                        />
+                        <InputGroupAddon align="inline-end">
+                            <SearchIcon/>
+                        </InputGroupAddon>
+                    </InputGroup>
+                </Field>
+            </div>
         </div>
     )
 }
